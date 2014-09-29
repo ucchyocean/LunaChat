5f4622798a79b5f788c120c7b9b1e25c04f084ae
ucchy <ocean163@hotmail.com>
2014-09-29 22:29:35 +0900

    LunaChat v2.6.9 : Fixed issue #108

--- src/main/java/com/github/ucchyocean/lc/channel/DelayedJapanizeConvertTask.java
+++ src/main/java/com/github/ucchyocean/lc/channel/DelayedJapanizeConvertTask.java
@@ -108,7 +108,7 @@ public class DelayedJapanizeConvertTask extends BukkitRunnable {
         for ( String keyword : keywords ) {
             if ( keywordLocked.contains(keyword) ) {
                 index++;
-                String key = "＜" + index + "＞";
+                String key = "＜" + makeMultibytesDigit(index) + "＞";
                 keywordLocked = keywordLocked.replace(keyword, key);
                 keywordMap.put(key, keyword);
             }
@@ -116,7 +116,7 @@ public class DelayedJapanizeConvertTask extends BukkitRunnable {
         for ( String dickey : dictionary.keySet() ) {
             if ( keywordLocked.contains(dickey) ) {
                 index++;
-                String key = "＜" + index + "＞";
+                String key = "＜" + makeMultibytesDigit(index) + "＞";
                 keywordLocked = keywordLocked.replace(dickey, key);
                 keywordMap.put(key, dictionary.get(dickey));
             }
@@ -125,16 +125,16 @@ public class DelayedJapanizeConvertTask extends BukkitRunnable {
         // カナ変換
         String japanized = KanaConverter.conv(keywordLocked);
 
-        // キーワードのアンロック
-        for ( String key : keywordMap.keySet() ) {
-            japanized = japanized.replace(key, keywordMap.get(key));
-        }
-
         // IME変換
         if ( type == JapanizeType.GOOGLE_IME ) {
             japanized = IMEConverter.convByGoogleIME(japanized);
         } else if ( type == JapanizeType.SOCIAL_IME ) {
             japanized = IMEConverter.convBySocialIME(japanized);
+        }
+
+        // キーワードのアンロック
+        for ( String key : keywordMap.keySet() ) {
+            japanized = japanized.replace(key, keywordMap.get(key));
         }
 
         // 変換後の文字列にNGワードが含まれている場合は、マスクする
@@ -170,6 +170,32 @@ public class DelayedJapanizeConvertTask extends BukkitRunnable {
      */
     public String getResult() {
         return result;
+    }
+
+    /**
+     * 数値を、全角文字の文字列に変換して返す
+     * @param digit
+     * @return
+     */
+    private String makeMultibytesDigit(int digit) {
+
+        String half = Integer.toString(digit);
+        StringBuilder result = new StringBuilder();
+        for ( int index=0; index < half.length(); index++ ) {
+            switch ( half.charAt(index) ) {
+            case '0' : result.append("０"); break;
+            case '1' : result.append("１"); break;
+            case '2' : result.append("２"); break;
+            case '3' : result.append("３"); break;
+            case '4' : result.append("４"); break;
+            case '5' : result.append("５"); break;
+            case '6' : result.append("６"); break;
+            case '7' : result.append("７"); break;
+            case '8' : result.append("８"); break;
+            case '9' : result.append("９"); break;
+            }
+        }
+        return result.toString();
     }
 }
 
