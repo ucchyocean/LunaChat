eeb1a289113cdb393eb72a1837c4867787b635ea
ucchy <ocean163@hotmail.com>
2015-04-11 13:32:07 +0900

    LunaChat v2.7.2 : Fixed issue #130

--- src/main/java/com/github/ucchyocean/lc/PlayerListener.java
+++ src/main/java/com/github/ucchyocean/lc/PlayerListener.java
@@ -14,6 +14,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -42,7 +43,7 @@ public class PlayerListener implements Listener {
      * プレイヤーのチャットごとに呼び出されるメソッド
      * @param event チャットイベント
      */
-    @EventHandler
+    @EventHandler(priority=EventPriority.HIGH, ignoreCancelled=true)
     public void onChat(AsyncPlayerChatEvent event) {
 
         LunaChatConfig config = LunaChat.getInstance().getLunaChatConfig();
