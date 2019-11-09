2fe4c2b960f61decc1dc045d5b2475c3f64f3e9e
LazyGon <yosihiro5225@gmail.com>
2019-11-09 12:40:24 +0900

    Removed Utility#callEventSync

--- src/main/java/com/github/ucchyocean/lc/Utility.java
+++ src/main/java/com/github/ucchyocean/lc/Utility.java
@@ -24,7 +24,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 
 /**
  * ユーティリティクラス
@@ -347,22 +346,5 @@ public class Utility {
     @SuppressWarnings("deprecation")
     public static Player getPlayerExact(String name) {
         return Bukkit.getPlayer(stripColor(name));
-    }
-
-    /**
-     * イベントを同期処理で呼び出します
-     *
-     * @param event 対象のイベント
-     * @return タスクのID (登録に失敗した場合は-1)
-     * @since 2.8.10
-     */
-    public static int callEventSync(final Event event) {
-        return Bukkit.getScheduler().scheduleSyncDelayedTask(LunaChat.getInstance(), new Runnable() {
-
-            @Override
-            public void run() {
-                Bukkit.getPluginManager().callEvent(event);
-            }
-        });
     }
 }
